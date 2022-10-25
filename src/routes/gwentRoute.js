8f8a5642f4b687ce6037ace20206ee2e17532880
Ricardo Maya <rickmaya@gmail.com>
2022-10-24 20:24:51 -0500

    get the card data from the gwent one api

--- src/routes/gwentRoute.js
+++ src/routes/gwentRoute.js
@@ -5,9 +5,17 @@ const axios = require('axios');
 router.get(`/gwent`, function (req, res) {
   axios.get('https://api.gwent.one/?key=data&version=10.10.1')
     .then((response) => {
+      // empty array to hold cards data
+      // let cardsArray = [];
       const cardData = response.data;
-      console.log(cardData);
+      // cardData.map((cards) => {
+      //   cardsArray.push(cards);
+      // });
+      // res.render('')
     })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
