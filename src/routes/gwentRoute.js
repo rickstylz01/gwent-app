d6d22ce25d34e41462995e3fb26f6d21392f8591
Ricardo Maya <rickmaya@gmail.com>
2022-10-27 17:06:47 -0500

    send json data from gwent api

--- src/routes/gwentRoute.js
+++ src/routes/gwentRoute.js
@@ -5,13 +5,8 @@ const axios = require('axios');
 router.get(`/gwent`, function (req, res) {
   axios.get('https://api.gwent.one/?key=data&version=10.10.1')
     .then((response) => {
-      // empty array to hold cards data
-      // let cardsArray = [];
       const cardData = response.data;
-      // cardData.map((cards) => {
-      //   cardsArray.push(cards);
-      // });
-      // res.render('')
+      res.send(cardData);
     })
     .catch((err) => {
       console.log(err);
