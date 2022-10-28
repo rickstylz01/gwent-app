58ccbc3cbe734a5df3126a1bd28a63d1efd01c46
Ricardo Maya <rickmaya@gmail.com>
2022-10-27 22:09:12 -0500

    place gwent one api url in a variable

--- src/routes/gwentRoute.js
+++ src/routes/gwentRoute.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const gwentOneUrl = 'https://api.gwent.one/?key=data&version=10.10.1'
 
 router.get(`/gwent`, function (req, res) {
-  axios.get('https://api.gwent.one/?key=data&version=10.10.1')
+  axios.get(gwentOneUrl)
     .then((response) => {
       const cardsArray = [];
       const cardData = response.data.response;
       cardsArray.push(cardData);
       res.send(cardsArray);
-      console.log(cardsArray[0][1].attributes.type);
+      console.log(cardsArray[0][1]);
     })
     .catch((err) => {
       console.log(err);
