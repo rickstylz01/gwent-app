8dfc1aa4919ac09058d63705763551a8680f74e5
Ricardo Maya <rickmaya@gmail.com>
2022-10-28 08:37:33 -0500

    use the response object instead of placing it in an array

--- src/routes/gwentRoute.js
+++ src/routes/gwentRoute.js
@@ -6,11 +6,9 @@ const gwentOneUrl = 'https://api.gwent.one/?key=data&version=10.10.1'
 router.get(`/gwent`, function (req, res) {
   axios.get(gwentOneUrl)
     .then((response) => {
-      const cardsArray = [];
-      const cardData = response.data.response;
-      cardsArray.push(cardData);
-      res.send(cardsArray);
-      console.log(cardsArray[0][1]);
+      const cardData = response.data.response["0"];
+      res.send(cardData);
+      // console.log(cardsArray[0][1]);
     })
     .catch((err) => {
       console.log(err);
