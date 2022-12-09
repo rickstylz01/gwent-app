009ba5886b7c07cb762defd630d010c4d8e99cc8
Ricardo Maya <rickmaya@gmail.com>
2022-12-08 19:10:28 -0600

    update Gwent version in url and delete comments

--- src/routes/gwentRoute.js
+++ src/routes/gwentRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const gwentOneUrl = 'https://api.gwent.one/?key=data&version=10.10.1'
+const gwentOneUrl = 'https://api.gwent.one/?key=data&version=10.12.0'
 
 router.get(`/gwent`, function (req, res) {
   axios.get(gwentOneUrl)
@@ -10,12 +10,6 @@ router.get(`/gwent`, function (req, res) {
       const keys = Object.keys(cardData);
       let cardNames = [];
       let cardValues = [];
-
-      // get the keys from the response object
-      // keys.forEach((key, index) => {
-        // console.log(cardData[key].name);
-        // cardNames.push(cardData[key].name);
-      // });
 
       // get the values from the response object
       Object.values(cardData)
