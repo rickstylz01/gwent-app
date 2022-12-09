ad131dd36213551e2f5c00468cfa436bedd2e615
Ricardo Maya <rickmaya@gmail.com>
2022-12-09 13:27:44 -0600

    seed gwent card data being retrieved from api into it's own json file

--- src/routes/gwentRoute.js
+++ src/routes/gwentRoute.js
@@ -7,11 +7,30 @@ router.get(`/gwent`, function (req, res) {
   axios.get(gwentOneUrl)
     .then((response) => {
       const cardData = response.data.response;
-      const keys = Object.keys(cardData);
-      let cardNames = [];
       let cardValues = [];
 
-      // get the values from the response object
+      // get the values from the response object and push into an array
+      Object.values(cardData)
+        .forEach(
+          val => cardValues.push(val)
+        );
+
+      res.send(cardValues);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get(`/search`, function (req, res) {
+  axios.get(gwentOneUrl)
+    .then((response) => {
+      const cardData = response.data.response;
+      // const keys = Object.keys(cardData);
+      // let cardNames = [];
+      let cardValues = [];
+
+      // get the values from the response object and push into an array
       Object.values(cardData)
         .forEach(
           val => cardValues.push(val)
